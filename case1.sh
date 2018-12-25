293360ec22a262706b4c7c2c550faf8027abca6e
Nagaraj <nagarajek16@gmail.com>
2018-12-25 16:57:01 +0530

    case file

---
+++ case1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+num=three 
+case $num in 
+	one) echo "this is one"
+		;;
+	two) echo "this is two"
+		;;
+	three) echo "this is three"
+		;;
+
+	four) echo "this is four"
+		;;
+	*) echo "this is invalid"
+		;;
+	
+		esac
+
